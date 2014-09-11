61267968ce779461e6d4d9aaa4a34919ce938832
Patrick Olsen <patrickolsen@sysforensics.org>
2014-09-11 13:43:50 +0800

    Update landesk-parse.py

--- landesk-parse.py
+++ landesk-parse.py
@@ -33,66 +33,31 @@ import base64, binascii, struct, sys
 import argparse
 from Registry import Registry
 from datetime import datetime, timedelta
+import csv
 
 def gethostInfo(reg_soft):
-    print ("\n" + ("=" * 51) + "\nHost Information\n" + ("=" * 51))
     entries = ["Wow6432Node\\LANDesk\\amtmon",
                 "LANDesk\\amtmon"]
     for en in entries:
         try:
             amtmon = reg_soft.open(en)
-            try:
+            if amtmon.value("ip").value() != None:
                 ip_addr = amtmon.value("ip").value()
-            except:
-                ip_addr = "No IP..."
-            try:
+            else:
+                ip_addr = "None"
+            if amtmon.value("hostname").value() != None:
                 host = amtmon.value("hostname").value()
-            except:
-                host = "No hostname...."
-
-            print 'Hostname: %s' % (host)
-            print 'IP Addr: %s' % (ip_addr)
+            else:
+                host = "None"
 
         except Registry.RegistryKeyNotFoundException as e:
-            pass
+            host = "None"
+            ip_addr = "None"
 
-def getlogonHist(reg_soft):
-    print ("\n" + ("=" * 51) + "\nLogin History\n" + ("=" * 51))
-    entries = ["Wow6432Node\\LANDesk\\Inventory\\LogonHistory",
-                "LANDesk\\Inventory\\LogonHistory"]
-    for en in entries:
-        try:
-            logon_hist = reg_soft.open(en)
-            for sks in logon_hist.subkeys():
-                if "logons" in sks.name().lower():
-                    ########################################################################
-                    # This is a terrible way to parse the groups, but it works for now...
-                    # I was getting unicode errors on the Asian group names in my test hive.
-                    # So I added the encode ignore in there for now...
-                    # You can remove the ''' and the pass and it should work for you.
-                    ########################################################################
-                    for v in sks.values():
-                        if "group" in v.name():
-                            pass
-                            '''
-                            g = v.value()
-                            for group in g:
-                                if 'CN' in group:
-                                    if ',OU' in group:
-                                        print '\t' + group.split('CN=')[1].split(',OU')[0].encode('ascii', 'ignore')
-                                    else:
-                                        print '\t' + group.split('CN=')[1].rstrip(',').encode('ascii', 'ignore')
-                            '''
-                        else:
-                            time = v.name()
-                            htime = datetime.fromtimestamp(int(time)).strftime('%Y-%m-%d %H:%M:%S')
-                            logins = '%s logged in at %s' % (v.value().partition(",")[0][3:], htime)
-                            print logins
-        except Registry.RegistryKeyNotFoundException as e:
-            pass
+    return host, ip_addr
 
-def getmonitorLog(reg_soft):
-    print ("\n" + ("=" * 51) + "\nApplication Log\n" + ("=" * 51))
+def getMonitorLog(reg_soft):
+    dic_Landesk = {}
     entries = ["Wow6432Node\LANDesk\ManagementSuite\WinClient\SoftwareMonitoring\MonitorLog",
                "LANDesk\\ManagementSuite\\WinClient\\SoftwareMonitoring\\MonitorLog"]
     
@@ -103,56 +68,55 @@ def getmonitorLog(reg_soft):
                 key = reg_soft.open(en+'\\%s' % (sks.name()))
                 app_name = key.name()
                 key_time = key.timestamp()
-                # The app name represents the key name.
-                print 'App Name: %s' % (app_name)
-                # LWrite = Last Write.
-                print 'Key LWrite: %s' % (key_time)
                 try:
                     time_convert = struct.unpack("<Q", key.value("Last Started").value())[0]
                     # http://stackoverflow.com/questions/4869769/convert-64-bit-windows-date-time-in-python
+                    # Convert this to a function and call it.
                     us = int(time_convert) / 10
                     last_run = datetime(1601,1,1) + timedelta(microseconds=us)
-                    print 'Last Run: %s' % (last_run)
                 except: 
-                    last_run = "No last runs..."
-                    print 'Last Run: %s' % (last_run)
+                    last_run = "None"
                 try:
                     time_convert = struct.unpack("<Q", key.value("First Started").value())[0]
+                    # Convert this to a function and call it.
                     us = int(time_convert) / 10
                     first_run = datetime(1601,1,1) + timedelta(microseconds=us)
-                    print 'First Run: %s' % (first_run)
                 except:
-                    first_run = "No first run..."
-                    print 'First Run: %s' % (first_run)
+                    first_run = "None"
                 try:
                     last_duration = struct.unpack("<Q", key.value("Last Duration").value())[0]
                     lduration = last_duration / 10000000
-                    print 'Last Duration: %s' % (lduration)
                 except:
-                    lduration = "No duration..."
-                    print 'Last Duration: %s' % (lduration)
+                    lduration = "None"
                 try:
                     total_duration = struct.unpack("<Q", key.value("Total Duration").value())[0]
                     tduration = total_duration / 10000000
-                    print 'Total Duration: %s' % (tduration)
                 except:
-                    tduration = "No duration..."
-                    print 'Total Duration: %s' % (tduration)
+                    tduration = "None"
                 try:
                     current_user = key.value("Current User").value()
-                    print 'Current User: %s' % (current_user)
                 except:
-                    current_user = "No user..."
-                    print 'Current User: %s' % (current_user)
+                    current_user = "None"
                 try:
                     run_runs = key.value("Total Runs").value()
-                    print 'Run count: %s' % (run_runs)
                 except:
-                    run_runs = "No run count..."
-                    print 'Run count: %s' % (run_runs)
-                print '\n'
+                    run_runs = "None"
+
+                dic_Landesk[app_name] = str(run_runs), str(key_time), str(first_run), str(last_run), \
+                                        str(lduration), str(tduration), current_user
+            return dic_Landesk
+
         except Registry.RegistryKeyNotFoundException as e:
             pass
+
+def outputResults(output, hosts):
+    LDwriter = csv.writer(sys.stdout)
+    LDwriter.writerow(["Application Name", "Host Name", "IP Address", "Total Runs", "Last Write", "First Run", \
+                        "Last Run", "Last Running Duration", "Total Running Duration", \
+                        "Curernt User"])
+    for key, value in output.iteritems():
+        LDwriter.writerow([key, hosts[0], hosts[1], value[0], value[1], value[2], \
+                            value[3], value[4], value[5], value[6]])
 
 def main():
     parser = argparse.ArgumentParser(description='Parse the Landesk Entries in the Registry.')
@@ -165,8 +129,8 @@ def main():
     else:
         print "You need to specify a SOFTWARE hive."
 
-    gethostInfo(reg_soft)
-    getlogonHist(reg_soft)
-    getmonitorLog(reg_soft)
+    hosts = gethostInfo(reg_soft)
+    output = getMonitorLog(reg_soft)
+    outputResults(output, hosts)
 if __name__ == "__main__":
     main()
